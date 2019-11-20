fb6a5d0e05312cd39e328fc8ec7f1df5238e85e7
maveeshah <mavee.shah@hotmail.com>
2019-11-20 15:52:10 +0500

    reverse

--- test.py
+++ test.py
@@ -15,9 +15,9 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-x = 1
-while(x):
+out = cv2.VideoWriter('test.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+while(True):
   ret, frame = cap.read()
 
   if ret == True:
@@ -26,10 +26,8 @@ while(x):
     out.write(frame)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-    x += 1
-    if x>100:
-        x = 0
+    # cv2.imshow('frame',frame)
+
     # Press Q on keyboard to stop recording
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
